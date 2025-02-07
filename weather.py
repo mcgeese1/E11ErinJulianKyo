98b9568baa3cf2b6676badef37186cda0ed1f7eb
mcgeese1 <e.mcgee@berkeley.edu>
2025-02-07 15:27:00 -0800

    First Edition of weather script

---
+++ weather.py
@@ -0,0 +1,18 @@
+#print('hello world')
+import adafruit_bme680
+import time
+import board
+
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+bme680.sea_level_pressure = 1013.25
+
+while True:
+		print("\nTemperature: %0.1f c" % bme680.temperature)
+		print("Gas: %d ohm" % bme680.gas)
+		print("Humidity: %0.1f %%" % bme680.relative_humidity)
+		print("Pressure: %0.3f hPa" % bme680.pressure)
+		print("altitude = %0.2f meters" % bme680.altitude)
+		
+		time.sleep(2)
